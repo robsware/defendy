34dd05fc08263b5bbac92707fea4857f8740226d
Rob <robert@cretu.org>
2020-07-19 20:31:06 +0300

    Added remove device API

--- webpage/js/getRenameDevices.js
+++ webpage/js/getRenameDevices.js
@@ -9,3 +9,14 @@ function pullListDevices(url) {
   xhttp.send();
 }
 
+function pullRemoveDevices(url) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById('pullRemoveDevices').innerHTML = this.responseText.replace(/\n/g, "<br />");
+    }
+  };
+  xhttp.open("GET", url + '?_=' + new Date().getTime(), true);
+  xhttp.send();
+}
+
--- webpage/textdata/renameDevice.py
+++ webpage/textdata/renameDevice.py
@@ -19,20 +19,20 @@ os.chdir(dname)
 
 @app.route('/rename', methods=['GET'])
 
-def my_route():
+def renameAPI():
 	oldDeviceName = request.args.get('oldName', default = 1, type = str)
 	newDeviceName = request.args.get('newName', default = 1, type = str)
-	print (oldDeviceName)
-	print (newDeviceName)
+	#print (oldDeviceName)
+	#print (newDeviceName)
 	oldDeviceName = oldDeviceName.split(" ", 1)[1]
 	with open('namedDevices.txt') as f:
 		namedDevices = f.readlines()
 		for num, line in enumerate(namedDevices, 1):
 			if oldDeviceName in line:
 				line = line.replace(oldDeviceName, newDeviceName)
-				print (namedDevices[num-1])
+				#print (namedDevices[num-1])
 				namedDevices[num-1] = line
-				print (namedDevices[num-1])
+				#print (namedDevices[num-1])
 
 	with open('namedDevices.txt', 'w') as f:
 		f.writelines(namedDevices)
@@ -69,27 +69,60 @@ def my_route():
 
 	return("Device renamed successfully")
 
-@app.route('/update', methods=['GET'])
+@app.route('/remove', methods=['GET'])
 def my_route1():
-	x = 1
-	x = x + 1
-	return(str(x))
+	removeDeviceName = request.args.get('removeName', default = 1, type = str)
 
 
+	removeDeviceName = removeDeviceName.split(" ", 1)[1]
+	with open('namedDevices.txt') as f:
+		namedDevices = f.readlines()
+		for num, line in enumerate(namedDevices, 1):
+			if removeDeviceName in line:
+				namedDevices.remove(namedDevices[num-1])
+
+
+	with open('namedDevices.txt', 'w') as f:
+		f.writelines(namedDevices)
+
+
+	displayDF = pd.read_csv('namedDevices.txt', names=["IP Address","MAC Address","Name","Date Added"], sep='\t')
+
+	displayDF.index += 1 
+	del displayDF["Date Added"]
+
+	displayDF.to_csv('displayDevices.txt', sep='\t')
+
+	with open("displayDevices.txt") as f:
+		lines = f.readlines()
+		lines[0] = "\t" + "IP Address" + "\t" + "MAC Address" + "\t" + "\t" +"Name" + "\n"
+
+	with open("displayDevices.txt", "w") as f:
+		f.writelines(lines)
+
+
+	del displayDF["MAC Address"]
+	displayDF.to_csv('renameDevices.txt', sep='\t', index=False, header=False)
+
+	with open("renameDevices.txt") as f:
+		lines = f.readlines()
+		lines = [line + '</option>' for line in lines]
+		lines = ['<option>' + line  for line in lines]
+
+
+	with open("renameDevices.txt", "w") as f:
+		f.writelines(lines)
+
+
+	return("Devices removed successfully")
+
+'''
 @app.route('/scan', methods=['GET'])
 def my_route2():
 
 
 
 	return("Scanning..")
+'''
 
 app.run(host="10.10.0.1")
-
-
-
-
-'''
-echo "* * * * * python3 /opt/defendy/webpage/python/getPublicIP.py" >> mycron
-echo "* * * * * python3 /opt/defendy/webpage/python/detect-devices.py" >> mycron
-echo "* * * * * python3 /opt/defendy/webpage/python/checkAlerts.py" >> mycron
-'''
