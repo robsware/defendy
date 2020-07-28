ff7df0df6208b0579e9683b82accd7a80f3bd61e
Rob <robert@cretu.org>
2020-07-28 21:55:23 +0300

    Added iptables rule generator

--- machinelearning/blockOutliers.py
+++ machinelearning/blockOutliers.py
@@ -4,38 +4,60 @@ import pandas as pd
 
 
 for outliersFile in os.listdir('.'):
-   if re.match('outliers', outliersFile):
-       IPForFiltering = outliersFile.split('_')[1]
-       IPForFiltering = IPForFiltering.split('.csv')[0]
-       outliersFileContent = pd.read_csv(outliersFile) 
-       print (outliersFileContent)
-       fieldsFilteredByIP = outliersFileContent[outliersFileContent['srcIP'].str.match(IPForFiltering)]
-       print (fieldsFilteredByIP)
-       for index, row in fieldsFilteredByIP.iterrows():
-           #with open("IPTablesRules_" + IPForFiltering, 'w'):
-           #print(row['srcIP'], row['srcPort'])
-           print("iptables -A OUTPUT -p tcp -d " + row['srcIP'] + " --destination-port " + str(row['srcPort']) + " -j DROP")
-            
-           #print(row['srcIP'], row['srcPort'])
+	if re.match('outliers', outliersFile):
+		IPForFiltering = outliersFile.split('_')[1]
+		IPForFiltering = IPForFiltering.split('.csv')[0]
+		outliersFileContent = pd.read_csv(outliersFile)
+		print (outliersFileContent)
+		rowsFilteredByIP = outliersFileContent[outliersFileContent['srcIP'].str.match(IPForFiltering)]
+		print (rowsFilteredByIP)
+		for index, row in rowsFilteredByIP.iterrows():
+			with open("IPTablesRules_" + IPForFiltering, 'a') as IPTablesRulesFile:
+				#IPTablesRulesFile.write("iptables -A OUTPUT -p tcp -d " + row['srcIP'] + " --destination-port " + str(row['srcPort']) + " -j DROP" + '\n')
 
-       
+				#os.system("iptables -A OUTPUT -p tcp -d " + row['srcIP'] + " --destination-port " + str(row['srcPort']) + " -j DROP")
+				print("iptables -A OUTPUT -p tcp -d " + row['srcIP'] + " --destination-port " + str(row['srcPort']) + " -j DROP")
+		#Rows that do not match an internal IP and come from the outside
+		rowsInboundBadTraffic = outliersFileContent[~outliersFileContent['srcIP'].str.match(IPForFiltering)]
+		for index, row in rowsInboundBadTraffic.iterrows():
+			with open("InboundIPTablesRles", 'a') as inboundTablesRulesFile:
+				#inboundTablesRulesFile.write("iptables -A INPUT -p tcp --destination-port " + str(row['srcPort']) +  " -s " + row['srcIP'] +" -j DROP")
+				print ("iptables -A INPUT -p tcp --destination-port " + str(row['srcPort']) +  " -s " + row['srcIP'] +" -j DROP")
 
 
-       #fieldsFilteredByIP[["srcIP", "srcPort", "dstIP", "dstPort"]].to_csv("ipTablesRules_" + ip + ".csv", index=None)
+
+#load rules in iptables
+for rulesFile in os.listdir('.'):
+	if re.match('IPTablesRules_', rulesFile):
+		with open (rulesFile, 'r') as rulesFileContent:
+			lines = rulesFileContent.readlines()
+			for line in lines:
+				print (line)
+				#os.system(line)
+		
+
+		   
+with open ("InboundIPTablesRles", 'r') as inboundRulesFile:
+	inboundRulesLines = inboundRulesFile.readlines()
+	for line in inboundRulesLines:
+		print (line)
+		#os.system(line)
+	   
 
 
 
 
+#TODO: when you remove a device, also delete iptables rules
 
 '''
 for file starting with outliers in name:
-    with open outliers
+	with open outliers
 
-        if srcIp is 10.10.something:
-            block outbound on srcIp and srcPort
-        elif srcIp is not 10.10.something:
-            block inbound on srcIp and srcPort
-        
+		if srcIp is 10.10.something:
+			block outbound on srcIp and srcPort
+		elif srcIp is not 10.10.something:
+			block inbound on srcIp and srcPort
+		
 
 Create file with new outbound rules
 Match ip and add button to remove all rules related to that ip
@@ -46,8 +68,8 @@ Button to reset inbount rules (delete that file)
 
 
 for ip in ips:
-    os.system("iptables -A INPUT -j DROP -s " + ip)
-    os.system("iptables -A INPUT -j DROP -s " + ip)
-    # iptables -A OUTPUT -p tcp -d 10.10.10.10 --destination-port 25 -j DROP
-    # iptables -A INPUT -p tcp --destination-port 8000 -s 1.2.3.4 -j DROP
+	os.system("iptables -A INPUT -j DROP -s " + ip)
+	os.system("iptables -A INPUT -j DROP -s " + ip)
+	# iptables -A OUTPUT -p tcp -d 10.10.10.10 --destination-port 25 -j DROP
+	# iptables -A INPUT -p tcp --destination-port 8000 -s 1.2.3.4 -j DROP
 '''
